6048646cd6aedbd6b62e1d47f9aa8dfced5e8b06
Abdón Rodríguez Davila <a@abdonrd.com>
2017-11-05 22:00:17 -0500

    Initial setup to Polydex project

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,12 @@
 /**
  * @license
- * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ *
+ * Copyright (c) 2017-present, Abdón Rodríguez Davila (@abdonrd).
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
  */
 
-/* eslint no-console: ["error", { allow: ["info"] }] */
+/* eslint-disable no-console */
 
-console.info(
-  'Service worker disabled for development, will be generated at build time.'
-);
+console.info('Service worker disabled for development, will be generated at build time.');
